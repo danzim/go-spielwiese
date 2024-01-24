9bd695e97cd556a2a4c12e861ee2ca2cc0c0495f
Daniel Zimny <daniel.zimny@huk-coburg.de>
2024-01-24 12:56:00 +0100

    remove files

--- api/v1/models/project.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type Project struct {
-	Ci          string `json:"ci"`
-	DisplayName string `json:"displayName" binding:"required"`
-	Description string `json:"description" binding:"required"`
-}
