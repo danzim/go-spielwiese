584a75890a82fae58f6e943f34e18601247b1662
danzim <danzim@dz-co.de>
2024-02-08 12:58:28 +0000

    initial commit

---
+++ internal/common/logger.go
@@ -0,0 +1,32 @@
+// internal/common/logger.go
+
+package common
+
+import (
+	"github.com/sirupsen/logrus"
+)
+
+var log *logrus.Logger
+
+func init() {
+	// Initialisiere den Logger
+	log = logrus.New()
+
+	// Setze das Log-Format auf JSON (kann je nach Bedarf angepasst werden)
+	log.SetFormatter(&logrus.JSONFormatter{})
+}
+
+// GetLogger gibt den initialisierten Logger zurück
+func GetLogger() *logrus.Logger {
+	return log
+}
+
+// Example of how to use the logger in another file:
+//
+// import "path/to/internal/common"
+//
+// func someFunction() {
+//     logger := common.GetLogger()
+//     logger.Info("This is an info message")
+//     logger.Error("This is an error message")
+// }
